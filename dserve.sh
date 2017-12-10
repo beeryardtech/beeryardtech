d2c93082d443a1df57044665dbb6f4618a079fb1
Travis Goldie <travis.goldie@steeleye.com>
2017-12-10 16:26:21 -0500

    Update dserver.sh to include `--user-data-dir`

--- dserve.sh
+++ dserve.sh
@@ -3,6 +3,12 @@
 # Author: Travis Goldie
 # Purpose: Start Jekyll Serve using dev config
 ###############################################################################
-bundle exec jekyll clean build
-/usr/bin/google-chrome-beta --disable-web-security --incognito --remote-debugging-port=9222 --ignore-certificate-errors _site/index.html
-bundle exec jekyll serve --verbose --watch  --config _config.yml,_config.dev.yml
+bundle exec jekyll clean 
+bundle exec jekyll build
+/usr/bin/google-chrome-beta --disable-web-security \
+    --incognito \
+    --remote-debugging-port=9222 \
+    --ignore-certificate-errors \
+    --user-data-dir \
+    _site/index.html
+bundle exec jekyll serve --verbose --watch --config _config.yml,_config.dev.yml
